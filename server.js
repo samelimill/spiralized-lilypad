b94e5aaffaba7159e1a72c7cfbce9ceaa3c2d446
samelimill <samelimill@gmail.com>
2023-10-10 14:07:56 -0700

    Added to server.js, got json to print on arrival at notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteData = require('./db/db.json');
-const api = require('./public/routes/notes.js')
+const api = require('./helpers/notes')
 const app = express();
 const PORT = 3001;
 
@@ -10,15 +10,23 @@ const PORT = 3001;
 app.use(express.json());
 app.use('/api', api);
 app.use(express.static('public'));
-// may need urlencoded here
+app.use(express.urlencoded({ extended: true }))
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/assets/index.html'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteData);
+});
+
+app.post('/api/notes', (req, res) => {
+    res.json(noteData);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
