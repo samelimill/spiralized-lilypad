b7260f4ff0404e588665ed7d931bf73dabe0a940
samelimill <samelimill@gmail.com>
2023-10-12 12:56:25 -0700

    Figured out promise issues, got post and delete to work

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const noteData = require('./db/db.json');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -21,11 +20,13 @@ app.get('/notes', (req, res) =>
 
 // get api/notes returns existing database
 app.get('/api/notes', (req, res) => {
-  res.status(200).json(noteData);
+  const noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(noteData);
 });
 
-// post api/notes reads the existing database and adds the new entry
+// post api/notes reads the existing database, adds a new note with id, and rewrites the db
 app.post('/api/notes', (req, res) => {
+  const noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   const note = req.body;
   note.id =  Math.floor((Math.random()) * 1000000).toString();
   noteData.push(note);
@@ -36,13 +37,14 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
+  const noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   const deleteId = req.params.id;
   const filteredData = noteData.filter((note) => note.id != deleteId);
   console.log(filteredData);
   fs.writeFileSync("./db/db.json", JSON.stringify(filteredData), function(err) {
-    if (err) throw (err);        
+    if (err) throw (err);
   });
-  res.json(filteredData); 
+  res.json(filteredData);
 });
 
 app.listen(PORT, () =>
