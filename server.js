6390a9b841df1e9a895a59c859f423213704b15d
samelimill <samelimill@gmail.com>
2023-10-11 20:13:05 -0700

    reworked server.js, deleted unneeded files

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteData = require('./db/db.json');
-const api = require('./helpers/notes')
+
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // middleware
 app.use(express.json());
-app.use('/api', api);
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 
+//paths for url
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -20,13 +20,30 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// get api/notes returns existing database
 app.get('/api/notes', (req, res) => {
-    res.json(noteData);
+  console.log(`${req.method} request received`);
+  res.status(200).json(noteData);
 });
 
+// post api/notes reads the existing database and adds the new entry
 app.post('/api/notes', (req, res) => {
-    res.json(noteData);
+  console.log('Note Added');
+  console.log(req.body);
+  const note = req.body;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    const notes = JSON.parse(data);
+    notes.push(note);
+    const updatedNotes = JSON.stringify(notes);
+    fs.writeFile('./db/db.json', updatedNotes, (err, data) =>
+    err ? console.error(err) : console.log('Notes updated')
+    )
+  });
 });
+
+app.delete('/api/notes', (req, res) => {
+  console.log('')
+})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
